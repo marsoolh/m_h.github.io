2544c0a36363fd5a739e18c2cb4b417672e35f4c
Muhammad Hoque <69122802+muhammadhoque@users.noreply.github.com>
2023-10-24 19:36:13 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,53 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const tabs = document.querySelectorAll('.tab');
+    const aboutTab = document.querySelector('.tab[data-section="about"]');
+    const educationTab = document.querySelector('.tab[data-section="education"]');
+    const experienceTab = document.querySelector('.tab[data-section="work"]');
+    const projectsTab = document.querySelector('.tab[data-section="projects"]');
+    // Remove the reference to the "Contact" tab
+    // const contactTab = document.querySelector('.tab[data-section="contact"]');
+    const contactForm = document.getElementById('contactForm');
 
-    tabs.forEach(tab => {
-        tab.addEventListener('click', () => {
-            const sectionId = tab.getAttribute('data-section');
-            const section = document.getElementById(sectionId);
-
-            if (section) {
-                window.scrollTo({
-                    top: section.offsetTop - 100, // Adjust this value as needed
-                    behavior: "smooth"
-                });
-            }
-        });
+    aboutTab.addEventListener('click', () => {
+        scrollToSection('about');
     });
+
+    educationTab.addEventListener('click', () => {
+        scrollToSection('education');
+    });
+
+    experienceTab.addEventListener('click', () => {
+        scrollToSection('work');
+    });
+
+    projectsTab.addEventListener('click', () => {
+        scrollToSection('projects');
+    });
+
+    // Remove the event listener for the "Contact" tab
+    /*
+    contactTab.addEventListener('click', () => {
+        scrollToSection('contact');
+    });
+    */
+
+    contactForm.addEventListener('submit', (e) => {
+        e.preventDefault(); // Prevent the default form submission
+        // Collect form data
+        const formData = new FormData(contactForm);
+        // Send the data to the specified email address (e.g., chat@gmail.com)
+        // You may use a server-side script to send the email.
+        // Example: fetch('send_email.php', { method: 'POST', body: formData });
+        // Replace 'send_email.php' with the actual server-side script.
+        alert('Message sent successfully!'); // For demonstration purposes
+    });
+
+    function scrollToSection(sectionId) {
+        const section = document.getElementById(sectionId);
+        if (section) {
+            window.scrollTo({
+                top: section.offsetTop - 100,
+                behavior: "smooth"
+            });
+        }
+    }
 });
