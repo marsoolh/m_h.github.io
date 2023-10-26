b7027cb666f7487f56aefdc591dbbd1e7cd12591
Muhammad Hoque <69122802+muhammadhoque@users.noreply.github.com>
2023-10-26 05:32:17 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,39 +1,16 @@
-// Define the scrollToSection function
-function scrollToSection(sectionId) {
+// Add an event listener to all tab elements
+document.querySelectorAll('.tab').forEach(tab => {
+    tab.addEventListener('click', scrollToSection);
+});
+
+// Function to scroll to the clicked section
+function scrollToSection(event) {
+    const sectionId = event.target.getAttribute('data-section');
     const section = document.getElementById(sectionId);
+
     if (section) {
-        section.scrollIntoView({ behavior: 'smooth' });
+        section.scrollIntoView({
+            behavior: 'smooth',
+        });
     }
 }
-
-document.addEventListener("DOMContentLoaded", function () {
-    const aboutTab = document.querySelector('.tab[data-section="about"]');
-    const educationTab = document.querySelector('.tab[data-section="education"]');
-    const experienceTab = document.querySelector('.tab[data-section="work"]');
-    const projectsTab = document.querySelector('.tab[data-section="projects"]');
-    const contactTab = document.querySelector('.tab[data-section="contact"]');
-    const contactForm = document.getElementById('contactForm');
-
-    aboutTab.addEventListener('click', () => {
-        scrollToSection('about');
-    });
-
-    educationTab.addEventListener('click', () => {
-        scrollToSection('education');
-    });
-
-    experienceTab.addEventListener('click', () => {
-        scrollToSection('work');
-    });
-
-    projectsTab.addEventListener('click', () => {
-        scrollToSection('projects');
-    });
-
-    contactTab.addEventListener('click', () => {
-        // Show the contact modal and load "contact.html" content
-        contactModal.style.display = 'block';
-        loadContactForm(); // Function to load contact form content
-    });
-
-   
