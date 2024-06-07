ef4809b6e8906fc8fc7c1d40fbdde2c157823ac3
Muhammad Hoque <muhammadmhoque@gmail.com>
2024-06-07 19:27:27 -0400

    updates for mobile-friendly browsing

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const tabs = document.querySelectorAll('.tab');
+    const tabs = document.querySelectorAll('.tab, .mobile-tab');
     const sections = document.querySelectorAll('.section');
+    const hamburgerMenu = document.querySelector('.hamburger-menu');
+    const mobileNav = document.querySelector('.mobile-nav');
 
     // Add an event listener to all tab elements
     tabs.forEach(tab => {
@@ -18,7 +20,17 @@ document.addEventListener('DOMContentLoaded', () => {
                 behavior: 'auto'
             });
         }
+
+        // Close the mobile nav after selection
+        if (mobileNav.classList.contains('active')) {
+            mobileNav.classList.remove('active');
+        }
     }
+
+    // Toggle the mobile nav menu
+    hamburgerMenu.addEventListener('click', () => {
+        mobileNav.classList.toggle('active');
+    });
 
     // Optionally, activate the first section by default
     if (sections.length > 0) {
